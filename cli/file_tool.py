2a9d324661045591545b7b74879ae920daa908ba
Extraordinarytechy <extraordinarytechy@gmail.com>
2025-07-12 22:40:09 +0530

    Fix: Clean formatting, spacing, and flake8 compliance in CLI tool and test

--- cli/file_tool.py
+++ cli/file_tool.py
@@ -1,16 +1,21 @@
 import argparse
 
+
 def count_lines(filename):
+    """Return the number of lines in the file."""
     with open(filename, 'r') as f:
         return len(f.readlines())
 
+
 def word_frequency(filename):
+    """Return a dictionary of word frequencies in the file."""
     with open(filename, 'r') as f:
         words = f.read().split()
     freq = {}
     for word in words:
         freq[word] = freq.get(word, 0) + 1
     return freq
+
 
 def main():
     parser = argparse.ArgumentParser(description='File CLI Tool')
@@ -23,6 +28,7 @@ def main():
         print(count_lines(args.filename))
     elif args.command == 'word-frequency':
         print(word_frequency(args.filename))
+
 
 if __name__ == '__main__':
     main()
--- tests/test_file_tool.py
+++ tests/test_file_tool.py
@@ -1,8 +1,9 @@
-import os
 from cli.file_tool import count_lines, word_frequency
+
 
 def test_count_lines():
     assert count_lines("test_input.txt") == 3
+
 
 def test_word_frequency():
     freq = word_frequency("test_input.txt")
